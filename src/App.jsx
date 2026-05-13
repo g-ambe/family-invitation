d67375692ede57bed9a5666558026f335601ecca
g-ambe <ranbo.ga@gmail.com>
2026-05-13 14:21:43 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { motion } from "framer-motion"
 
 import heroImage from "./assets/images/hero.jpg"
 import leavesImage from "./assets/images/leaves.png"
-import coupleImage from "./assets/images/couple.jpeg"
+import coupleImage from "./assets/images/couple3.jpg"
+//import coupleImage from "./assets/images/couple.jpeg"
 
 export default function App() {
   const targetDate = new Date("2026-05-24T11:00:00")
